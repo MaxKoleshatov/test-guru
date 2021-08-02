2632777a310888075b3e8a3250acca3999f54e9f
Max <maksimkoleshatov1993@gmail.com>
2021-08-03 00:29:44 +0500

    lesson_3_Rails_model_migration_fix_2

--- db/migrate/20210802133311_add_answers_1.rb
+++
@@ -1,8 +0,0 @@
-class AddAnswers1 < ActiveRecord::Migration[6.1]
-  def change
-    change_column :answers, :correct, :boolean
-    add_column :answers, :answer_client, :string
-    add_reference :answers, :question, index: true, null: false
-    add_foreign_key :answers, :questions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_133311) do
+ActiveRecord::Schema.define(version: 2021_08_02_183240) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
+    t.string "answer_client", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "answer_client"
-    t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 2021_08_02_133311) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,5 +55,5 @@ ActiveRecord::Schema.define(version: 2021_08_02_133311) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
-  add_foreign_key "tests", "users"
+  add_foreign_key "tests", "users", column: "author_id"
 end
