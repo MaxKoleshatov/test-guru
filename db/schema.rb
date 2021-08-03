c1a4a346a9fb1c339a4994c1e45a7133ae3e1d5d
Max <maksimkoleshatov1993@gmail.com>
2021-08-03 14:27:40 +0500

    lesson_3_Rails_model_migration_fix_3

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_183240) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
-    t.string "answer_client", null: false
+    t.string "text", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
