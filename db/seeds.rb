58616eea48d98fe2fcd69124974e45a716898636
Max <maksimkoleshatov1993@gmail.com>
2021-08-07 00:15:34 +0500

    Lesson_4_fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-users = User.create([{name: "Макс"}, {name: "Денис"}, {name: "Павел"}])
+users = User.create!([{name: "Макс"}, {name: "Денис"}, {name: "Павел"}])
 
-categories = Category.create([{title: "Спорт"}, {title: "История"}, {title: "Кино"}])
+categories = Category.create!([{title: "Спорт"}, {title: "История"}, {title: "Кино"}])
 
-tests = Test.create([
-    {title: "Футбол", level: "1", category_id: categories[0].id, author_id : users[0].id},
-    {title: "Хоккей", level: "1", category_id: categories[0].id, author_id : users[0].id},
-    {title: "Наполеон", level: "3", category_id: categories[1].id, author_id : users[1].id},
-    {title: "Вторая мировая война", level: "1", category_id: categories[1].id, author_id : users[1].id},
-    {title: "Оскар", level: "5", category_id: categories[2].id, author_id : users[2].id}
+tests = Test.create!([
+    {title: "Футбол", level: "1", category_id: categories[0].id, author_id: users[0].id},
+    {title: "Наполеон", level: "3", category_id: categories[1].id, author_id: users[1].id},
+    {title: "Оскар", level: "5", category_id: categories[2].id, author_id: users[2].id}
 ])
 
-questions = Question.create([
+questions = Question.create!([
     {body: "Какая страна была первым ЧМ по футболу?", test_id: tests[0].id},
-    {body: "В каком году родился Наполеон Бонапарт?", test_id: tests[2].id},
-    {body: "По скольки номинациям вручается премия Оскар?", test_id: tests[4].id}
+    {body: "В каком году родился Наполеон Бонапарт?", test_id: tests[1].id},
+    {body: "По скольки номинациям вручается премия Оскар?", test_id: tests[2].id}
 ])
 
-answers = Answer.create([
+answers = Answer.create!([
     {text: "Аргентина", correct: false, question_id: questions[0].id},
     {text: "Португалия", correct: false, question_id: questions[0].id},
     {text: "Уругвай", correct: true, question_id: questions[0].id},
@@ -34,5 +32,5 @@ answers = Answer.create([
     {text: "1771", correct: false, question_id: questions[1].id},
     {text: "20", correct: false, question_id: questions[2].id},
     {text: "26", correct: false, question_id: questions[2].id},
-    {text: "24", correct: false, question_id: questions[2].id},
+    {text: "24", correct: true, question_id: questions[2].id},
 ])
