4f520685d4ae09c6358bb522d8a47569195a6c9f
Max <maksimkoleshatov1993@gmail.com>
2021-08-10 22:36:13 +0500

    lesson_5_Диаграмма_Асоциации_fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :tests
   has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :tests, through: :tests_users, dependent: :destroy
+
   def user_tests_by_level(level)
-    Test.joins('JOIN tests_users ON tests_users.test_id = tests.id').where(level: level).where(tests_users: { user_id: id })
+    Test.joins(:tests_users).where(level: level).where(tests_users: { user_id: id })
   end
 end
