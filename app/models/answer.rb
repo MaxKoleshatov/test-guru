c24ebaef24f5b79547b460f54b73886a4104175f
Max <maksimkoleshatov1993@gmail.com>
2021-08-21 12:08:36 +0500

    Lesson_6_fix_2

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,18 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correct, -> {where(correct: true)}
-
   validates :text, presence: true
+  validate :answers_limit, on: :create
 
-  validate :answers_limit
+  scope :correct, -> {where(correct: true)}
 
   private
 
   def answers_limit
     if question.answers.count >= 4
       errors.add(:question, "Ответов должно быть не больше 4")
-    else
     end
   end
 end
