da4ca964781fc23e672ad37f7e06b7978a5e646e
Max <maksimkoleshatov1993@gmail.com>
2021-08-25 21:10:38 +0500

    lesson_7_Controllers_fix_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   belongs_to :category
 
+  
   has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
