fa49ab4d8033e95accc94c78626fe46e533b7891
Max <maksimkoleshatov1993@gmail.com>
2021-08-25 22:35:46 +0500

    lesson_7_Controllers_new_branch_1

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,6 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   belongs_to :category
 
-  
   has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: 'questions#index'
-  
-  resources :tests do
-    resources :questions
-  end
-
-=======
   resources :tests do
     resources :questions, shallow: true
   end
->>>>>>> lesson_7_Controllers
 end
 # For details on the DSL available within this file, see https://guides.r
