e35c1f94a7aac5b57f3a26ed5882a26fc0bad30b
Max <maksimkoleshatov1993@gmail.com>
2021-11-04 22:00:03 +0500

    lesson_17_fix_2

--- db/migrate/20210916114441_change_user_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserName < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :name, :first_name
-  end
-end
--- db/migrate/20210916183004_add_last_name_and_type_to_users.rb
+++ db/migrate/20210916183004_add_last_name_and_type_to_users.rb
@@ -1,8 +1,12 @@
 class AddLastNameAndTypeToUsers < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :users, :first_name, :string
     add_column :users, :type, :string, null: false, default: 'User'
     add_column :users, :last_name, :string
     add_index :users, :type
+  end
+
+  def down
+    change_column :users, :first_name, :text
   end
 end
